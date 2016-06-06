49448b7b268a7e84c8b2685b377c58aab31cbd0c
vatsan1986 <srivatsan231@gmail.com>
2016-06-06 03:44:17 +0200

    Add files via upload

---
+++ Registration.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+var myapp = angular.module("apptrainer", ['firebase']);
+myapp.controller("registerController", ['$scope','$firebaseObject',function($scope,$firebaseObject) {
+	
+	$scope.register= function(event) {
+		var firebaseObj = new Firebase("https://apptrainer1.firebaseio.com/");
+		var fb = $firebaseObject(firebaseObj);
+
+		fb.name= $scope.username;
+					fb.age=$scope.age;
+					fb.Height=$scope.height;
+					fb.password=$scope.password;
+					fb.sex=$scope.sex;
+					fb.weight=$scope.weight;
+					fb.email=$scope.email;
+					fb.phone=$scope.phone;
+				    fb.Dateofbirth=$scope.date;
+					fb.secretQuestion=$scope.question;
+					fb.secretanswer=$scope.answer;
+				fb.$save().then(function(ref) {
+					console.log(ref);
+
+				}, function(error) {
+					console.log("Error:", error);
+				});
+	};
+}]);
